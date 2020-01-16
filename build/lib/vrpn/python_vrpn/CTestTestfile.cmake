fd2b33c0f9701ff1c268643dd33785203ef42bc5
sarahaguasvivas <saag5228@colorado.edu>
2020-01-16 12:42:46 -0700

    built sample code using cmake

---
+++ build/lib/vrpn/python_vrpn/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sarahaguasvivas/gpc_controller/lib/vrpn/python_vrpn
+# Build directory: /home/sarahaguasvivas/gpc_controller/build/lib/vrpn/python_vrpn
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
