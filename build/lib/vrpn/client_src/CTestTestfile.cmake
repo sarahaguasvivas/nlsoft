5103acc8b0ce30a63ebb4e2720217a5243e4d39d
sarahaguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2020-01-16 20:17:18 -0700

    deleting build file

--- build/lib/vrpn/client_src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sarahaguasvivas/gpc_controller/lib/vrpn/client_src
-# Build directory: /home/sarahaguasvivas/gpc_controller/build/lib/vrpn/client_src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/lib/vrpn/python/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sarahaguasvivas/gpc_controller/lib/vrpn/python
-# Build directory: /home/sarahaguasvivas/gpc_controller/build/lib/vrpn/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
