548625f88033c68889609f06000d0c13bece1704
sarahaguasvivas <saag5228@colorado.edu>
2020-01-16 17:07:39 -0700

    current state

--- src/optitrack/vrpn_client.cpp
+++ src/optitrack/vrpn_client.cpp
@@ -4,18 +4,11 @@ VRPNclient::VRPNclient(std::vector<std::string> tracker_name, std::string host_I
     this->host_IP= host_IP;
     for (int i=0; i< tracker_name.size(); i++)
         this->tracker_name.push_back(tracker_name[i]);
-    this->connection = new vrpn_Connection();
-    long float = connection->register_message_type(float);
-    long id = connection->register_sender(host_IP);
 
-    struct timeval now;
-    gettimeofday(&now, NULL):
-
-    connection->pack_message(sizeof("VRPN Connection established!"), new, float, id, "VRPN Connection established!", vrpn_CONNECTION_RELIABLE);
-    connection->mainloop();
+    this->Tracker = new vrpn_Tracker_Remote("D-Head@192.168.50.33", NULL);
 }
 
-VRPNclient::start_connection(){
+void VRPNclient::start_connection(){
      
 }
 
