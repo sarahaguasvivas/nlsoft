b45c4ce04a4f46d962643609a5bc84547a1aff7c
sarahaguasvivas <saag5228@colorado.edu>
2020-01-19 14:39:24 -0700

    progress

--- optitrack/optitrack.py
+++ optitrack/optitrack.py
@@ -3,11 +3,11 @@ import vrpn
 def callback(userdata, data):
     print userdata + "=> " +  data
 
-
 class VRPNclient:
     def __init__(self, tracker_name, hostID):
         self.tracker_name = tracker_name
         self.hostID= hostID
+
         self.tracker = vrpn.receiver.Tracker(tracker_name + "@" + hostID)
         self.tracker.register_change_handler("tracker", callback, "position")
 
@@ -22,10 +22,10 @@ class VRPNclient:
         self.analog.mainloop()
         self.button.mainloop()
 
-
 if __name__=='__main__':
-    C = VRPNclient("D-Head", "192.168.50.33:3883")
-    B = VRPNclient("D-Base", "192.168.50.33:3883")
+    C = VRPNclient("D-Head", "192.168.50.33")
+    B = VRPNclient("D-Base", "192.168.50.33")
+
     while True:
         C.sample_data()
         B.sample_data()
