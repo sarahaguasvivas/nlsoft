4cbe423949b78e555405c62e0fc16b0fc6ba73c3
sarahaguasvivas <saag5228@colorado.edu>
2020-01-23 10:01:43 -0700

    gym working. Ready to collect data

--- gym/block_gym.py
+++ gym/block_gym.py
@@ -3,37 +3,49 @@ from signals.serial_client import *
 from dynamixels.dynamixels import *
 
 class BlockGym():
-    def __init__(self, sensor_port= "/dev/ttyACM0"):
+    def __init__(self, sensor_port= "/dev/ttyACM0",  \
+                            motors_port = "/dev/ttyUSB0", \
+                                vrpn_ip = "192.168.50.33:3883"):
         self.observation = []
         self.verbose = 1
         self.terminal = False
         self.reward = 0
         self.info = ""
-        self.sensor_port = sensor_port
-        self.dynamixels = DynamixelActor(port = self.sensor_port)
-        self.sensor_signals = OpticalSignal()
-        self.block_or = BlockOrientation()
 
-    def step(self, action):
+        self.motors_port = motors_port
+        self.motors = DynamixelActor(port = self.motors_port)
+
+        self.sensor_signals = OpticalSignal()
+
+        self.state = None
+
+        self.vrpn_ip = vrpn_ip
+
+        self.block_or = BlockOrientation(ip = self.vrpn_ip)
+
+        self.target = self.block_or.get_target()
+
+    def step(self, action = [0, 0]):
         # return observation, reward, done, info
-        pass
+        self.motors.step(action = action)
 
     def reset(self):
         # return to zero position for both servos
-        pass
+        self.motors.reset()
 
     def get_info(self):
         # get info wrt the types of verbose
-        pass
+        from_motors = self.motors.get_info()
 
     def is_terminal(self):
         pass
 
     def get_observation(self):
         # use signal reader from Arduino
+        print "getting observation"
         end_eff = self.block_or.get_observation()
-        print(end_eff)
-        pass
+        signals = self.sensor_signals.collect_signal_sample()
+        return  signals + end_eff
 
     def reward(self):
         # Calculate cost
@@ -41,13 +53,13 @@ class BlockGym():
 
     def done(self):
         # reset
-
-        pass
-
+        self.sensor_signals.close_network()
+        self.motors.close_connection()
 
 if __name__ == "__main__":
     B = BlockGym()
 
     while True:
-        B.get_observation()
+        print "HERE"
+        print "Observation: ", B.get_observation()
 
