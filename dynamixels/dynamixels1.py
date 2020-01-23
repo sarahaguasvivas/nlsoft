288d898c6710e6ca23ffbc4fdf5f2a3715635262
sarahaguasvivas <saag5228@colorado.edu>
2020-01-22 17:30:38 -0700

    need to work on dynamixel library

--- dynamixels/dynamixels1.py
+++ dynamixels/dynamixels1.py
@@ -1,4 +1,5 @@
-from pyax12.connection import Connection
+#from lib.dynamixel_hr.dxl.dxlchain import DxlChain
+import sys
 
 class DynamixelActor:
     def __init__(self, serial_port = '/dev/ttyUSB0', baudrate = 57600, verbose = True):
@@ -7,8 +8,7 @@ class DynamixelActor:
 
         self.baudrate = baudrate
         self.serial_port = serial_port
-        self._serial_connection = Connection(port=serial_port, \
-                                    baudrate = self.baudrate)
+        self._serial_connection =DxlChain(self.serial_port, rate = self.baudrate)
         self._zero1= 0
         self._zero2= -50
 
@@ -25,23 +25,29 @@ class DynamixelActor:
     def step(self, action=[0, 0]):
         self._angle1 , self._angle2 = action
         self._serial_connection.goto(int(self._dynamixel1), \
-                            self._angle1, speed = 512, degrees = True)
+                            self._angle1, speed = 512)
         self._serial_connection.goto(int(self._dynamixel2), \
-                            self._angle2, speed = 512, degrees = True)
+                            self._angle2, speed = 512)
 
     def reset(self):
         self._angle1= self._zero1
         self._angle2= self._zero2
         self._serial_connection.goto(int(self._dynamixel1), \
-                            self._zero1, speed = 512, degrees = True)
+                            self._zero1, speed = 512)
         self._serial_connection.goto(int(self._dynamixel2), \
-                            self._zero2, speed = 512, degrees = True)
+                            self._zero2, speed = 512)
 
     def get_info(self):
         self.__info = "Dynamixels: ID1= " + str(self._angle1) + \
-                                " degrees;ID2= " + str(self._angle2) + " degrees"
+                                " degrees; ID2= " + str(self._angle2) + " degrees"
         return self.__info
 
     def close_connection(self):
         self._serial_connection.close()
         return True
+
+
+if __name__=='__main__':
+    motors = DynamixelActor()
+    motors.reset([0, 0])
+
