4c4981d44370ce082f50e97ffbf8c6c394c5cfaa
sarahaguasvivas <saag5228@colorado.edu>
2020-01-23 16:40:06 -0700

    fixed data collection

--- signals/serial_client.py
+++ signals/serial_client.py
@@ -23,16 +23,23 @@ class OpticalSignal:
         self.data = []
         while len(self.data) != self.num_sensors:
             x = []
-            while len(x) < 60 or len(x) > 68:
+            array_string = []
+
+            while len(x) < 60 or len(x) > 68 and len(array_string) != self.num_sensors:
                 x = self.serial_.readline()
-            print len(x), x
-            array_string = x.split(",")
+                array_string = x.split(",")
+            print x
             array_string[-1] = array_string[-1].replace("\n", "")
             array_string[-1] = array_string[-1].replace("\r", "")
             array_string[-1] = array_string[-1].replace("/n", "")
             array_string[-1] = array_string[-1].replace("n", "")
             array_string[-1] = array_string[-1].replace("..", ".")
             array_string[0] = array_string[0].replace("..", ".")
+
+            while array_string[0][0] == ",":
+                array_string[0][0] = ""
+            while array_string[0][0] == ".":
+                array_string[0][0] = ""
 
             for ii in array_string:
                 try:
@@ -42,12 +49,20 @@ class OpticalSignal:
                         array_string[ii][0] = ""
                     while array_string[ii][0] == ".":
                         array_string[ii][0] = ""
+
                     array_split = array_string[ii].split(".")
-                    if len(array_split) > 1:
+                    if len(array_split) > 2:
                         chunk1 = array_string[:ii]
                         chunk2 = array_string[ii+1:]
                         array_string = chunk1 + [str(float(array_split[0]))] + \
-                                            [str(float(array_split[1]))] + chunk2
+                                            [str(float(array_split[2]))] + chunk2
+
+                    while array_string[ii][0] == ",":
+                        array_string[ii][0] = ""
+                    while array_string[ii][0] == ".":
+                        array_string[ii][0] = ""
+
+            print array_string
             self.data = [float(i) for i in array_string]
         return self.data
 
--- test/data_collection.py
+++ test/data_collection.py
@@ -7,15 +7,16 @@ B = BlockGym()
 B.reset()
 
 actions1 = list(np.arange(130, -150, -0.3))
-actions2 = list(np.arange(150, -150, -0.3))
+actions2 = list(np.arange(130, -150, -0.3))
 
 today = date.today()
-f = open("data_" + today.strftime("%b_%d_%Y_6")+".txt", "w+")
+f = open("data_" + today.strftime("%b_%d_%Y_7")+".txt", "w+")
 
 try:
     for i in actions1:
         for j in actions2:
             B.step(action=[i, j])
+            #time.sleep(0.5)
             print i, j
             data = B.get_observation() + B.get_real_position()
             data = data + [i, j]
