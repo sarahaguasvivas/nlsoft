84d5a0e40eb732674b401b5eb502aad8455debc9
sarahaguasvivas <saag5228@colorado.edu>
2020-01-25 14:51:42 -0700

    need sig calibration

--- test/data_collection.py
+++ test/data_collection.py
@@ -1,29 +1,44 @@
+#!/usr/bin/env python2.7
 from gym.block_gym import *
 import time
 from datetime import date
+import traceback, sys
 
 B = BlockGym()
 
 B.reset()
 # 102.7 -42.8
 
-actions1 = list(np.arange(102.7, -150, -0.3))
+actions1 = list(np.arange(150, -150, -0.3))
 actions2 = list(np.arange(130, -150, -0.3))
+actions3 = list(np.arange(-150, 130, 0.3))
+
+flag = True
 
 today = date.today()
 f = open("data_" + today.strftime("%b_%d_%Y_8")+".txt", "w+")
 
 try:
     for i in actions1:
-        for j in actions2:
-            B.step(action=[i, j])
-            #time.sleep(0.5)
-            print i, j
-            data = B.get_observation() + B.get_real_position()
-            data = data + [i, j]
-            print >> f, data
+        if flag:
+            for j in actions2:
+                B.step(action=[i, j])
+                #time.sleep(0.5)
+                print i, j
+                data = B.get_observation() + B.get_real_position()
+                data = data + [i, j]
+                print >> f, data
+        if not flag:
+            for j in actions3:
+                print i, j
+                B.step(action = [i, j])
+                data = B.get_observation() + B.get_real_position()
+                data = data + [i, j]
+                print >> f, data
+        flag = not flag
 
     B.reset()
-except:
+except Exception as e:
+    print str(e)
     f.close()
     B.reset()
