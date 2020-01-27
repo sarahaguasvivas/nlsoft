f0c4f0490c00f8e960e49b0b9a23f8507d6e3ca3
sarahaguasvivas <saag5228@colorado.edu>
2020-01-27 08:53:02 -0700

    proceeding to train nn

--- test/gpc_controller.py
+++ test/gpc_controller.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python2.7
-#from gym.block_gym import * # vrpn not installed in mac
+from gym.block_gym import * # vrpn not installed in mac
 from controller.dynamic_model import *
 from controller.newton_raphson import *
 import numpy as np
 import os
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-
 #plt.style.use('dark_background')
-filename = "/Users/sarahaguasvivas/gpc_controller/data/model_data.csv"
+filename = str(os.environ["HOME"]) + "/gpc_controller/data/model_data.csv"
+
+
+
+
 
 def prepare_data_file(filename = '../data/model_data.csv', nd = 3, dd = 3):
     data_array = np.genfromtxt(filename, delimiter=',')
     signals = data_array[:, :11]
-    position = data_array[:, 11:17]
+    position = data_array[:, 11:14] # not using Euler angles
     inputs = data_array[:, 17:]
+
     N = max(nd, dd) # data sample where we will start first
     U = np.empty((signals.shape[0] - N + 1, 2))
     Y = np.empty((signals.shape[0] - N + 1, 3))
@@ -30,16 +32,18 @@ def prepare_data_file(filename = '../data/model_data.csv', nd = 3, dd = 3):
 
     U = U[:, 2:]
     S = signals[N - 1:, :]
-    Y = Y[:, 3:] # Y
+    Y = Y[:, 3:-3] # Y
 
     X = np.concatenate((U, S), axis = 1)
-    return X, Y
+    X = np.concatenate((X, Y[:, 3:]), axis = 1)
 
+    y = Y[:, :3] # we are using all of this to estimate current
 
+    return X, y
 
 
 if __name__ == "__main__":
-    prepare_data_file(filename, nd=3, dd=3)
+    X, y = prepare_data_file(filename, nd=3, dd=3)
 
 
 
---
+++ test/reset.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+from gym.block_gym import *
+import time
+from datetime import date
+import traceback, sys
+
+B = BlockGym()
+
+B.reset()
+
+today = date.today()
+f = open("data_" + today.strftime("%b_%d_%Y_8")+".txt", "w+")
+# my_string = ','.join(map(str, my_list))
+
+B.reset()
