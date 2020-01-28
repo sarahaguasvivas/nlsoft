b0b007787e0dbc925388ab58509b224b0f81637d
sarahaguasvivas <saag5228@colorado.edu>
2020-01-28 13:25:25 -0700

    testing controller

--- test/gpc_controller.py
+++ test/gpc_controller.py
@@ -18,6 +18,8 @@ Block.reset()
 #Block.stretch() # stretch block for better signal readings before calibrating
 #Block.get_signal_calibration() # calibrate signal of the block
 
+Block.calibration_max = np.array([ 30, 390,  54,   6,   7,   1, 247,   1,  28, 153,  16 ])
+
 u_optimal_old = np.array([0., -50.])
 new_state_new = Block.get_state()
 
@@ -37,17 +39,19 @@ ym = []
 state = []
 yn = []
 
-# Remember I need to change m to mm
-for n in range(1):
+# Working in mm
+for n in range(100):
     seconds = time.time()
 
-    signal = Block.get_observation()
+    signal = np.divide(Block.get_observation(), Block.calibration_max, dtype = np.float64).tolist()
+
     neural_network_input = np.array(signal + np.array(list(u_deque)).flatten().tolist() +\
                                     np.array(list(y_deque)).flatten().tolist())
 
     neural_network_input = np.reshape(neural_network_input, (1, -1))
 
     predicted_states = NNP.predict(neural_network_input).flatten()
+
     NNP.yn = predicted_states
 
     NNP.ym = Block.get_target() * 1000
@@ -55,8 +59,8 @@ for n in range(1):
     new_state_old = new_state_new
 
     u_optimal = np.reshape(NR_opt.optimize(u = u_optimal_old, del_u = du, rtol = 1e-8, \
-                    maxit = 6, verbose = False)[0], (-1, 1))
-    print u_optimal
+                    maxit = 6, verbose = False)[0], (-1, 1)).flatten()
+
     u_optimal_list += [u_optimal.flatten().tolist() ]
 
     du = np.array(u_optimal_old) - np.array(u_optimal)
@@ -65,5 +69,12 @@ for n in range(1):
 
     u_optimal_old = u_optimal
 
+    Block.step(u_optimal.tolist())
+
+    u_deque.pop()
+    y_deque.pop()
+    u_deque.appendleft(u_optimal.tolist())
+    y_deque.appendleft(predicted_states.tolist())
+
     # need to keep adding stuff. Also need to have deque with previous signals and states
-    Block.reset()
+Block.reset()
