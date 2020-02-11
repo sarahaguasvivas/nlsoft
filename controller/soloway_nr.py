8929d9eccfdf1915d5419c91c181c7ba6c414e5d
sarahaguasvivas <saag5228@colorado.edu>
2020-02-11 15:13:54 -0700

    need to test controller

--- controller/soloway_nr.py
+++ controller/soloway_nr.py
@@ -18,25 +18,22 @@ class SolowayNR:
         norm0 = np.linalg.norm(Fu)
         enorm_last= np.linalg.norm(U - np.ones(U.shape))
 
-        for i in range(maxit):
-            Ju = self.d_model.Ju()
+#        for i in range(maxit):
+        Ju = self.d_model.Ju()
+        delU =  -np.dot(np.linalg.inv(Ju),  Fu)
+        U += delU
+        Fu = self.d_model.Fu()
+        norm = np.linalg.norm(Fu)
 
-            delU =  -np.dot(np.linalg.inv(Ju),  Fu)
+        if verbose:
+            enorm = np.linalg.norm(U - np.ones(U.shape))
+            print "Newton: ", i, " anorm: ", norm, " rnorm: ", norm/norm0, " eratio: ", enorm/enorm_last**2
+            enorm_last = enorm
 
-            U += delU
-
-            Fu = self.d_model.Fu()
-            norm = np.linalg.norm(Fu)
-
-            if verbose:
-                enorm = np.linalg.norm(U - np.ones(U.shape))
-                print "Newton: ", i, " anorm: ", norm, " rnorm: ", norm/norm0, " eratio: ", enorm/enorm_last**2
-                enorm_last = enorm
-
-            if norm < rtol * norm0:
-                break
+           # if norm < rtol * norm0:
+           #     break
 
         return U, delU
 
     def optimize(self, u = [0, 0], del_u=[0,0], verbose=False):
-       return self.__fsolve_newton(u, del_u, verbose)
+       return self.__fsolve_newton(u0 = u, del_u = del_u,  verbose = verbose)
