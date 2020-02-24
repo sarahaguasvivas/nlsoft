2e1702c29035320021be88a9c050c9b63d51824d
sarahaguasvivas <saag5228@colorado.edu>
2020-02-24 13:23:33 -0700

    working controller need to tune

--- test/tuning_gpc_controller.py
+++ test/tuning_gpc_controller.py
@@ -52,85 +52,82 @@ for _ in range(NNP.dd):
     y_deque.append(Block.get_state())
 
 n = 0
-#try:
+try:
 # working in m
-while True:
-    seconds = time.time()
+    while True:
+        seconds = time.time()
 
-    signal = np.divide(Block.get_observation(), Block.calibration_max, dtype = np.float64).tolist()
+        signal = np.divide(Block.get_observation(), Block.calibration_max, dtype = np.float64).tolist()
 
-    neural_network_input = np.array(signal + np.array(list(u_deque)).flatten().tolist() + \
-                                    np.array(list(y_deque)).flatten().tolist())
+        neural_network_input = np.array(signal + np.array(list(u_deque)).flatten().tolist() + \
+                                        np.array(list(y_deque)).flatten().tolist())
 
-    neural_network_input = np.reshape(neural_network_input, (1, -1))
+        neural_network_input = np.reshape(neural_network_input, (1, -1))
 
-    predicted_states = NNP.predict(neural_network_input).flatten() / 1000.
+        predicted_states = NNP.predict(neural_network_input).flatten() / 1000.
 
-    NNP.yn = predicted_states
+        NNP.yn = predicted_states
 
-    NNP.ym = np.array([neutral_point[0], neutral_point[1] , \
-                                        neutral_point[2] + 0.1 * sig.square(np.pi * n / 50.) - 0.1/2.])
+        NNP.ym = np.array([neutral_point[0], neutral_point[1] , \
+                                            neutral_point[2] +  sig.square(np.pi * n / 50.)])
 
-    new_state_old = new_state_new
+        new_state_old = new_state_new
 
-    u_optimal, del_u = NR_opt.optimize(u = u_optimal_old, del_u = del_u, \
-                                        maxit = 1, rtol = 1e-8, verbose = False)
+        u_optimal, del_u = NR_opt.optimize(u = u_optimal_old, del_u = del_u, \
+                                            maxit = 1, rtol = 1e-8, verbose = False)
 
 
-    u_optimal = u_optimal[0, :].tolist()
+        u_optimal = u_optimal[0, :].tolist()
 
-    del_u = del_u[0, :].tolist()
+        del_u = del_u[0, :].tolist()
 
-    #u_optimal[0] = np.clip(u_optimal[0], -300, 150)
-    #u_optimal[1] = np.clip(u_optimal[1], -300, 150)
+        u_optimal[0] = np.clip(u_optimal[0], -300, 150)
+        u_optimal[1] = np.clip(u_optimal[1], -300, 150)
 
-    u_optimal[0] %= 250.
-    u_optimal[1] %= 250.
+        print "-----------------------------------------------------"
+        print "GPC: Target: ", NNP.ym
+        print "GPC: P. State: ", NNP.yn
+        print "GPC: Act. State: ", Block.get_state()
+        print "GPC: u_optimal", u_optimal
+        print "GPC: Cost: ", NNP.compute_cost()
 
-    print "-----------------------------------------------------"
-    print "GPC: Target: ", NNP.ym
-    print "GPC: P. State: ", NNP.yn
-    print "GPC: Act. State: ", Block.get_state()
-    print "GPC: u_optimal", u_optimal
-    print "GPC: Cost: ", NNP.compute_cost()
+        NNP.update_dynamics(u_optimal_old, del_u, NNP.yn.tolist(), NNP.ym.tolist())
 
-    NNP.update_dynamics(u_optimal_old, del_u, NNP.yn.tolist(), NNP.ym.tolist())
+        u_optimal_old = u_optimal
 
-    u_optimal_old = u_optimal
+        Block.step(action = u_optimal)
 
-    Block.step(action = u_optimal)
+        u_deque.pop()
+        y_deque.pop()
 
-    u_deque.pop()
-    y_deque.pop()
+        u_deque.appendleft(u_optimal)
+        y_deque.appendleft(predicted_states.tolist())
 
-    u_deque.appendleft(u_optimal)
-    y_deque.appendleft(predicted_states.tolist())
+        ym += [NNP.ym]
+        yn += [NNP.yn]
 
-    ym += [NNP.ym]
-    yn += [NNP.yn]
+        elapsed += [time.time() - seconds]
 
-    elapsed += [time.time() - seconds]
+        print "GPC: elapsed time: ", elapsed[-1]
+        print ""
 
-    print "GPC: elapsed time: ", elapsed[-1]
-    print ""
+        u_optimal_list+= [u_optimal]
+        actual_states += [Block.get_state()]
+        n += 1
 
-    u_optimal_list+= [u_optimal]
-    actual_states += [Block.get_state()]
-    n += 1
+except:
+    Block.reset()
+    ym = np.reshape(ym, (-1, 3))
+    yn = np.reshape(yn, (-1, 3))
+    actual_states = np.reshape(actual_states, (-1, 3))
+    u_optimal_list = np.reshape(u_optimal_list, (-1, 2))
 
-#except:
-#    Block.reset()
-#    ym = np.reshape(ym, (-1, 3))
-#    yn = np.reshape(yn, (-1, 3))
-#    actual_states = np.reshape(actual_states, (-1, 3))
-#    u_optimal_list = np.reshape(u_optimal_list, (-1, 2))
-#
-#    labels = ['x', 'y', 'z']
-#    for i in range(3):
-#        plt.subplot(3, 1, i+1)
-#        plt.plot(ym[:, i]*1000, '--w', label = 'target')
-#        plt.plot(yn[:, i]*1000, 'cyan', label = 'predicted state')
-#        plt.plot(actual_states[:, i]*1000, label = 'actual state')
-#        plt.legend()
-#        plt.ylabel(str(labels[i]) + ' [mm]')
-#    plt.show()
+    labels = ['x', 'y', 'z']
+    for i in range(3):
+        plt.subplot(3, 1, i+1)
+        plt.plot(ym[:, i]*1000, '--w', label = 'target')
+        plt.plot(yn[:, i]*1000, 'cyan', label = 'predicted state')
+        plt.plot(actual_states[:, i]*1000, label = 'actual state')
+        plt.legend()
+        plt.ylabel(str(labels[i]) + ' [mm]')
+    plt.show()
