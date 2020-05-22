c1450c99a0caf81746bbe34a264d6254607113f4
sarahaguasvivas <saag5228@colorado.edu>
2020-05-22 10:59:15 -0600

    this works better

--- test/tuning_gpc_controller.py
+++ test/tuning_gpc_controller.py
@@ -19,10 +19,13 @@ verbose = 0
 
 NNP = NeuralNetworkPredictor(model_file = model_filename, N1 = 0, \
                 N2 = 1, Nu = 1, nd = 2, dd = 2, K = 3, \
-                    Q = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
-                    R = np.array([[3e1, 0.], [0., 2e2]]), \
+                    Q = np.array([[5e-1, 0, 0],
+                                    [0, 3e-1, 0.],
+                                    [0, 0., 5.5]]),
+                    R = np.array([[3e-7, 1e-10],
+                                    [1e-10, 7e-8]]), \
                         y0 = [0.02, -0.05, 0.05], \
-                            u0 = [0.0, -50.0], s = 1e-20, b = 5e-7, r = 4e-5)
+                            u0 = [0.0, -50.0], s = 1e-10, b = 1e-10, r = 4e-1)
 
 NR_opt, Block = SolowayNR(cost = NNP.cost, d_model = NNP), \
                         BlockGym(vrpn_ip = "192.168.50.24:3883")
@@ -30,7 +33,7 @@ log = Logger()
 Block.reset()
 
 neutral_point = Block.get_state()
-target = Ellipse(wavelength = 50, amplitude = 0.03, center = neutral_point)
+target = Ellipse(wavelength = 50, amplitude = 15./1000., center = neutral_point)
 
 #Block.get_signal_calibration() # calibrate signal of the block
 Block.calibration_max = np.array([ 6, 377, 116,   1,   1,   1, 137,   1,   1,  41,   1 ])
@@ -45,60 +48,67 @@ log.log({'metadata' : {'neutral_point' : neutral_point,\
 
 u_deque, y_deque = first_load_deques(neutral_point, NNP.u0, NNP.nd, NNP.dd)
 u_action, predicted_states = np.array(NNP.u0), np.array(new_state_new)
-for e in range(NUM_EXPERIMENTS):
-    log.log({str(e) : {'predicted' : [], 'actual' : [], 'yn' : [], \
-            'ym' : [], 'elapsed' : [], 'u' : []}})
+try:
+    for e in range(NUM_EXPERIMENTS):
+        log.log({str(e) : {'predicted' : [], 'actual' : [], 'yn' : [], \
+                'ym' : [], 'elapsed' : [], 'u' : []}})
 
-    Block.reset()
-    for n in range(NUM_TIMESTEPS):
-        seconds = time.time()
-        signal = np.divide(Block.get_observation(), Block.calibration_max, \
-                            dtype = np.float64).tolist()
-        NNP.yn = []
-        for k in range(NNP.K):
-            neural_network_input = np.array((np.array(list(u_deque)) \
-                                                /100.).flatten().tolist() + \
-                                                np.array(list(y_deque)
-                                                    ).flatten().tolist() + \
-                                                    signal).reshape(1, -1)
-            predicted_states = NNP.predict(neural_network_input).flatten()
-            NNP.yn += [predicted_states]
+        Block.reset()
+        for n in range(NUM_TIMESTEPS):
+            seconds = time.time()
+            signal = np.divide(Block.get_observation(), Block.calibration_max, \
+                                dtype = np.float64).tolist()
+            NNP.yn = []
+            for k in range(NNP.K):
+                neural_network_input = np.array((np.array(list(u_deque)) \
+                                                    /100.).flatten().tolist() + \
+                                                    np.array(list(y_deque)
+                                                        ).flatten().tolist() + \
+                                                        signal).reshape(1, -1)
+                predicted_states = NNP.predict(neural_network_input).flatten()
+                NNP.yn += [predicted_states]
+                y_deque = roll_deque(y_deque, predicted_states.tolist())
+
+            NNP.ym = target.spin(n, NNP.N1, NNP.N2, 3, predicted_states.tolist())
+            new_state_old = new_state_new
+            u_optimal, del_u,  _ = NR_opt.optimize(u = u_optimal_old, \
+                                        maxit = 1, rtol = 1e-4, verbose = True)
+            print 'del_u',  del_u
+            u_action = u_optimal[0, :].tolist()
+            del_u_action = del_u[0, :].tolist()
+
+            u_action[0] = normalize_and_clip_angle(u_action[0],-100, 100)
+            u_action[1] = normalize_and_clip_angle(u_action[1],-100, 60)
+
+            Block.step(action = u_action)
+            NNP.update_dynamics(u_optimal[0, :].tolist(), del_u_action, predicted_states.tolist(), \
+                                        NNP.ym[0, :].tolist())
+            u_optimal_old = u_optimal
+            u_deque = roll_deque(u_deque, u_optimal[0, :].tolist())
             y_deque = roll_deque(y_deque, predicted_states.tolist())
 
-        NNP.ym = target.spin(n, NNP.N1, NNP.N2, 3, predicted_states.tolist())
-        new_state_old = new_state_new
-        u_optimal, del_u,  _ = NR_opt.optimize(u = u_optimal_old, \
-                                    maxit = 1, rtol = 1e-4, verbose = True)
-        print 'del_u',  del_u
-        u_action = u_optimal[0, :].tolist()
-        del_u_action = del_u[0, :].tolist()
+            if verbose == 0:
+                log.verbose(actual = np.array(Block.get_state()).tolist(),
+                        yn = predicted_states, ym = NNP.ym[0, :], \
+                            elapsed = time.time()-seconds, u = u_action)
+            if verbose == 1:
+                log.verbose(u = u_action, cost = NNP.cost.compute_cost())
 
-        u_action[0] = normalize_and_clip_angle(u_action[0],-100, 100)
-        u_action[1] = normalize_and_clip_angle(u_action[1],-100, 60)
+            log.log({str(e) : {'actual' : np.array(Block.get_state()).tolist(), \
+                            'yn' : predicted_states.tolist(), \
+                            'ym' : NNP.ym[0, :].tolist(),\
+                            'elapsed' : time.time() - seconds,\
+                            'u' : [u_action], 'cost : ': NNP.cost.compute_cost(), \
+                            'signal' : signal}})
 
-        Block.step(action = u_action)
-        NNP.update_dynamics(u_optimal[0, :].tolist(), del_u_action, predicted_states.tolist(), \
-                                    NNP.ym[0, :].tolist())
-        u_optimal_old = u_optimal
-        u_deque = roll_deque(u_deque, u_optimal[0, :].tolist())
-        y_deque = roll_deque(y_deque, predicted_states.tolist())
-
-        if verbose == 0:
-            log.verbose(actual = np.array(Block.get_state()).tolist(),
-                    yn = predicted_states, ym = NNP.ym[0, :], \
-                        elapsed = time.time()-seconds, u = u_action)
-        if verbose == 1:
-            log.verbose(u = u_action, cost = NNP.cost.compute_cost())
-
-        log.log({str(e) : {'actual' : np.array(Block.get_state()).tolist(), \
-                        'yn' : predicted_states.tolist(), \
-                        'ym' : NNP.ym[0, :].tolist(),\
-                        'elapsed' : time.time() - seconds,\
-                        'u' : [u_action], 'cost : ': NNP.cost.compute_cost(), \
-                        'signal' : signal}})
-
-    u_optimal_old = np.reshape(NNP.u0*NNP.Nu, (-1, 2))
+        u_optimal_old = np.reshape(NNP.u0*NNP.Nu, (-1, 2))
+        Block.reset()
+    log.plot_log()
+    log.save_log()
+except Exception as e:
+    import traceback
+    print str(e)
     Block.reset()
-log.plot_log()
-log.save_log()
+    Block.motors.close_connection()
+    traceback.print_exc(file = sys.stdout)
 
