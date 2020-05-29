956c321eebb6ba9b76789ef9eb02da059e7b6ca4
sarahaguasvivas <saag5228@colorado.edu>
2020-05-29 15:56:33 -0600

    tuning for faster control loop time

--- test/tuning_gpc_controller.py
+++ test/tuning_gpc_controller.py
@@ -15,19 +15,19 @@ model_filename = str(os.environ['HOME']) + '/gpc_controller/test/sys_id.hdf5'
 NUM_EXPERIMENTS = 1
 NUM_TIMESTEPS = 1000
 
-SCALE0 = 100.
-SCALE1 = 100.
+SCALE0 = 150.
+SCALE1 = 150.
 
 verbose = 0
 
 #neutral point
 NNP = NeuralNetworkPredictor(model_file = model_filename,
-                    N1 = 0, N2 = 2, Nu = 1, nd = 2, dd = 2, K = 2,
-                    Q = 5e3*np.array([[0.7, 0.],
+                    N1 = 0, N2 = 2, Nu = 1, nd = 2, dd = 2, K = 5,
+                    Q = 5e-1*np.array([[0.7, 0.],
                                        [0, 0.5e1]]),
-                    Lambda = np.array([[5.5e-7]]),
+                    Lambda = np.array([[5.5e-10]]),
                         y0 = [0.0, 0.00, 0.0],
-                        u0 = [0.0, 0.0], s = 1e-10, b = 1e-5, r = .4)
+                        u0 = [0.0, 0.0], s = 1e-10, b = 1e-5, r =4.)
 
 NR_opt, Block = SolowayNR(cost = NNP.cost, d_model = NNP), \
                         BlockGym(vrpn_ip = "192.168.50.24:3883")
