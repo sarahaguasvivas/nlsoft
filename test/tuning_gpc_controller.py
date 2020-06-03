0bd91efb68c71ae379fed5c73a7846ecab9194a0
sarahaguasvivas <saag5228@colorado.edu>
2020-06-03 03:39:56 -0600

    fixed optitrack readings

--- test/tuning_gpc_controller.py
+++ test/tuning_gpc_controller.py
@@ -16,10 +16,10 @@ NUM_EXPERIMENTS = 1
 NUM_TIMESTEPS = 1000
 
 SCALE0 = 100.
-SCALE1 = 100.
+SCALE1 = 1.
 
 verbose = 0
-
+#-0.055511750280857086, -0.04026632010936737, -0.018170952796936035
 #neutral point
 NNP = NeuralNetworkPredictor(model_file = model_filename,
                     N1 = 0, N2 = 2, Nu = 1, nd = 2, dd = 2, K = 5,
@@ -27,18 +27,18 @@ NNP = NeuralNetworkPredictor(model_file = model_filename,
                                        [0, 0.5e1]]),
                     Lambda = np.array([[5.5e-10]]),
                         y0 = [0.0, 0.00, 0.0],
-                        u0 = [0.0, 0.0], s = 1e-10, b = 1e-5, r =4.)
+                        u0 = [0.0, -50.0], s = 1e-10, b = 1e-5, r =4.)
 
 NR_opt, Block = SolowayNR(cost = NNP.cost, d_model = NNP), \
                         BlockGym(vrpn_ip = "192.168.50.24:3883")
 log = Logger()
 Block.reset()
-Block.step([0.0, 0.])
+Block.step([0.0, -50.])
 neutral_point = Block.get_state()
-
+print "neutral_point: ", neutral_point
 #NNP.y0 = neutral_point
 
-target = Pringle2(wavelength = 100, amplitude = 15./1000., center = neutral_point)
+target = Pringle2(wavelength = 1000, amplitude = 15./1000., center = neutral_point)
 
 #Block.get_signal_calibration() # calibrate signal of the block
 Block.calibration_max = np.array([ 1, 288, 110,   1,   1,   1, 104,   1,   1,  38,   1 ])
@@ -91,11 +91,11 @@ try:
             del_u_action = del_u[0, :].tolist()
 
             # clipping for safety; with good tuning this is almost never needed:
-            u_action[0] = np.clip(SCALE0*u_action[0],-100, 80)
-            u_action[1] = np.clip(SCALE1*u_action[1],-100, 60)
+            #u_action[0] = np.clip(SCALE0*u_action[0],-100, 80)
+            #u_action[1] = np.clip(SCALE1*u_action[1],-100, 60)
 
-            #u_action[0] = np.clip(50*np.sin(2.*np.pi*n/ 100.), -100, 80)
-            #u_action[1] = np.clip(50*np.cos(2.*np.pi*n/100.), -100, 60)
+            u_action[0] = np.clip(100*np.sin(2.*np.pi*n/ 100.), -100, 80)
+            u_action[1] = np.clip(100*np.cos(2.*np.pi*n/100.), -100, 60)
             Block.step(action = u_action)
             NNP.update_dynamics(u_optimal[0, :].tolist(), del_u_action, \
                                 predicted_states.tolist(), NNP.ym[0, :].tolist())
