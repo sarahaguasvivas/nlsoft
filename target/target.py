44be7634b34dc01678aa7cbd6e7058dbd6f6909e
sarahaguasvivas <saag5228@colorado.edu>
2020-06-03 15:29:52 -0600

    tuning new model

--- target/target.py
+++ target/target.py
@@ -58,19 +58,19 @@ class Pringle2:
     def spin(self, timestep, n1, n2, dims, current_point):
         target = np.empty([n2 - n1, dims])
 
-        #phase = self.find_projection_along_path(current_point)
-        phase = 0
+        phase = self.find_projection_along_path(current_point)
         i = 0
 
         for _ in range(n1, n2):
-            y = 0.0 #self.amplitude * np.sin(2.*np.pi*(timestep + i) \
+            y = -9./1000. #self.amplitude * np.sin(2.*np.pi*(timestep + i) \
                 #                                        / (self.wavelength) + phase) + 0./1000.
             z = self.amplitude *np.cos(2*np.pi*(timestep + i)/ \
                                                         (self.wavelength) + phase) - 0./1000.
 
-            x = 0.0/1000. #self.amplitude * np.sin(1000.*z*y) + 0./1000.
+            x = -2.0/1000. #self.amplitude * np.sin(1000.*z*y) + 0./1000.
 
             target[i, :] = [self.center[0] + x, self.center[1] + y, self.center[2] + z]
+
             i+=1
         return target
 class Pringle:
