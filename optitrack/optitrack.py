fixed second derivative issue

optitrack/optitrack.py
@@ -56,10 +56,10 @@ class BlockOrientation():
         print "Optitrack Comm Initialized!"
 
     def get_observation(self):
-        head_o = self.head.get_observation()[:3] + np.array([-2./1000., -2./1000., 0.])
-        base_o = self.base.get_observation()[:3] + np.array([-2./1000., -3./1000., 0.])
-        v = (np.array(head_o) - np.array(base_o))
-        Rot = R.from_rotvec(np.array([0.895,0,0])) # coming from off centroids
+        head_o = self.head.get_observation()[:3]
+        base_o = self.base.get_observation()[:3]
+        v = (np.array(head_o) - np.array(base_o)) + np.array([0.0, -7.92/1000., 1.177/1000.])
+        Rot = R.from_rotvec(np.array([0.85, -0.1435,0.0305])) # coming from off centroids
         ob=  Rot.apply(v).tolist()
         return ob
 