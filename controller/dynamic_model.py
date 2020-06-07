fixed second derivative issue

controller/dynamic_model.py
@@ -64,8 +64,8 @@ class NeuralNetworkPredictor():
         self.dd = dd # associated with y( . )
         self.Hessian = None
 
-        self.previous_first_der = 0.0 # important for recursions
-        self.previous_second_der = 0.0 # important for recursions
+        self.previous_first_der = 1.0 # important for recursions
+        self.previous_second_der = 1.0 # important for recursions
 
         self.y_deque = deque()
         self.delu_deque = deque()
@@ -100,6 +100,8 @@ class NeuralNetworkPredictor():
         self.delu_deque.pop()
         self.ym_deque.pop()
 
+        self.prediction = None
+
         self.u_deque.appendleft(u)
         self.delu_deque.appendleft(del_u)
         self.y_deque.appendleft(y)
@@ -115,21 +117,26 @@ class NeuralNetworkPredictor():
         return Y, YM, U, delU
 
     def __Phi_prime(self, x = 0):
+
         if self.model.layers[-1].get_config()['activation'] == 'linear':
             return np.array([1.0]*self.num_y) # linear activation on output
         if self.model.layers[-1].get_config()['activation'] == 'tanh':
-            return  1. / np.cosh(x)**2.
+            netj = np.arctanh(self.prediction)
+            return  np.array(1. / np.cosh(netj)**2.)
         if self.model.layers[-1].get_config()['activation'] == 'sigmoid':
-            sigmoid = 1./(1.+np.exp(-1.*x))
-            return sigmoid*(1-sigmoid)
+            netj = np.ln(self.prediction / (1-self.prediction))
+            sigmoid = 1./(1.+np.exp(-1.*netj))
+            return np.array(sigmoid*(1-sigmoid))
 
     def __Phi_prime_prime(self, x = 0):
         if self.model.layers[-1].get_config()['activation'] == 'linear':
             return np.array([0.0]*self.num_y) # linear activation on output
         if self.model.layers[-1].get_config()['activation'] == 'tanh':
-            return-2.*np.tanh(x)/ np.cosh(x)**2.
+            netj = np.arctanh(self.prediction)
+            return np.array(-2.*np.tanh(netj)/ np.cosh(netj)**2.)
         if self.model.layers[-1].get_config()['activation'] == 'sigmoid':
-            return (2.*np.exp(-2.*x))/(np.exp(-1.*x)+ 1.)**3. - (np.exp(-1.*x))/(np.exp(-1.*x)+1.)**2.
+            x = np.ln(self.prediction / (1-self.prediction))
+            return np.array((2.*np.exp(-2.*x))/(np.exp(-1.*x)+ 1.)**3. - (np.exp(-1.*x))/(np.exp(-1.*x)+1.)**2.)
 
     def __partial_2_fnet_partial_nph_partial_npm(self, h, m, j):
         """
@@ -148,14 +155,13 @@ class NeuralNetworkPredictor():
                  D^2yn
             ---------------
             Du(n+h) Du(n+m)
-            TODO: This should be a num_y x num_y matrix
         """
         weights = self.model.layers[-1].get_weights()[0]
         sum_output = 0.0
         for i in range(self.hid):
             for j in range(weights.shape[1]):
-                sum_output+= np.multiply(weights[i, j], \
-                            self.__partial_2_fnet_partial_nph_partial_npm(h, m, j))
+                sum_output+= weights[i, j] * \
+                            self.__partial_2_fnet_partial_nph_partial_npm(h, m, j)
         self.previous_second_der = sum_output
         return np.array(sum_output)
 
@@ -189,7 +195,8 @@ class NeuralNetworkPredictor():
         weights = self.model.layers[-1].get_weights()[0]
         sum_output = np.array([0.0]*weights.shape[1])
         for i in range(self.hid):
-            sum_output += np.dot(weights[i, :] , self.__partial_fnet_partial_u(h, j))
+            sum_output += np.dot(np.array(weights[i, :]) ,
+                            self.__partial_fnet_partial_u(h, j).T)
         self.previous_first_der = sum_output.tolist()
         return np.array(sum_output)
 
@@ -251,24 +258,32 @@ class NeuralNetworkPredictor():
                 for j in range(self.N1, self.N2):
                     ynu  += [self.__partial_yn_partial_u(j, m)]
                     ynu1 += [self.__partial_yn_partial_u(j, h)]
-                    temp += [[1, 1, 1]]
-                ynu, ynu1, temp = np.array(ynu), np.array(ynu1), np.array(temp)
+                    temp += [self.__partial_2_yn_partial_nph_partial_npm(h, m, j)]
 
-                Hessian[h, m] += np.sum(2.*self.Q.dot(np.array(ynu).dot(np.array(ynu1).T)) - self.Q.dot((YM[self.N1:self.N2, :] - Y[self.N1:self.N2, :])).dot(np.array(temp).T))
+                ynu, ynu1, temp = np.array(ynu), np.array(ynu1), \
+                        np.array(temp).reshape(-1, self.num_y)
+
+                Hessian[h, m] += np.sum(2.*ynu.T.dot(self.Q).dot(np.array(ynu1)) - \
+                        self.Q.dot(YM[self.N1:self.N2, :] - \
+                        Y[self.N1:self.N2, :]).T.dot(np.array(temp)))
+
                 second_y, second_y1, temp = [], [], []
+
                 for j in range(self.Nu):
                     second_y+=[self.__partial_delta_u_partial_u(j, m)]
                     second_y1+= [self.__partial_delta_u_partial_u(j, h)]
-                Hessian[h, m] += np.sum(2.*np.dot(self.Lambda, second_y).dot(np.array(second_y1).T), axis = 0)
+                Hessian[h, m] += np.sum(2.*np.dot(self.Lambda, \
+                                second_y).dot(np.array(second_y1).T), axis = 0)
 
                 for j in range(self.Nu):
                     for i in range(self.num_u):
                         Hessian[h, m] += kronecker_delta(h, j)*kronecker_delta(m, j) * \
-                                (2.0*self.constraints.s / np.power((U[j, i] + self.constraints.r / 2. - \
+                                (2.0*self.constraints.s / np.power((U[j, i] + \
+                                self.constraints.r / 2. - \
                                 self.constraints.b), 3.0) + \
                                 2.0 * self.constraints.s / np.power(self.constraints.r/2. +\
                                 self.constraints.b - U[j, i], 3.0))
-        #print "Hessian: ", Hessian
+        print "Hessian: ", Hessian
         return Hessian
 
     def compute_jacobian(self, u, del_u):
@@ -325,8 +340,8 @@ class NeuralNetworkPredictor():
         pass
 
     def predict(self, x):
-        return self.model.predict(x, batch_size=1)
-
+        self.prediction = self.model.predict(x, batch_size=1)
+        return  self.prediction
 
 
 

target/target.py
@@ -62,12 +62,12 @@ class Pringle2:
         i = 0
 
         for _ in range(n1, n2):
-            y = -9./1000. #self.amplitude * np.sin(2.*np.pi*(timestep + i) \
-                #                                        / (self.wavelength) + phase) + 0./1000.
-            z = self.amplitude * np.sin(2*np.pi*(timestep + i)/ \
+            y = 0.0#self.amplitude * np.sin(2.*np.pi*(timestep + i) \
+                #                                        / (self.wavelength) + phase) - 0./1000.
+            z = self.amplitude * np.cos(2*np.pi*(timestep + i)/ \
                                                         (self.wavelength) + phase) - 0./1000.
 
-            x = -2.0/1000. #self.amplitude * np.sin(1000.*z*y) + 0./1000.
+            x =0.0 #self.amplitude * np.sin(100.*z*y) - 0./1000.
 
             target[i, :] = [self.center[0] + x, self.center[1] + y, self.center[2] + z]
 

test/training_sys_int.py
@@ -17,13 +17,13 @@ import keras.backend as K
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 import random
-NUM_DATA_RUNS = 300
+NUM_DATA_RUNS = 100
 TRAIN = True
 
 #plt.style.use('dark_background')
 plt.style.use('seaborn')
-filename = str(os.environ["HOME"]) + "/gpc_controller/data/model_data11.csv"
-filename1 = str(os.environ["HOME"]) + "/gpc_controller/data/model_data12.csv"
+filename = str(os.environ["HOME"]) + "/gpc_controller/data/model_data13.csv"
+filename1 = str(os.environ["HOME"]) + "/gpc_controller/data/model_data14.csv"
 
 def custom_loss(y_true, y_pred):
     return 1000*K.mean(K.square(y_pred - y_true), axis = -1)
@@ -31,15 +31,14 @@ def custom_loss(y_true, y_pred):
 keras.losses.custom_loss = custom_loss
 def neural_network_training(X, y):
     #y = 1000*y
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
     model = Sequential()
-    model.add(Dense(100, activation =  'tanh', kernel_initializer='random_normal'))
-    #model.add(GaussianNoise(0.1))
-    model.add(Dense(3,  activation = 'linear', kernel_initializer='random_normal'))
+    model.add(Dense(15, activation =  'linear', kernel_initializer='random_normal'))
+    model.add(Dense(3,  activation = 'tanh', kernel_initializer='random_normal'))
 
     model.compile(optimizer= 'adam', loss =custom_loss, metrics=['mse'])
-    model.fit(X_train, y_train, epochs = 100, batch_size = 100, validation_split=0.2)
+    model.fit(X_train, y_train, epochs = 5000, batch_size = 1000, validation_split=0.2)
     print model.predict(X_test)
     model.save('sys_id.hdf5')
     return 'sys_id.hdf5'
@@ -107,7 +106,8 @@ def plot_sys_id(X, y, modelfile= 'sys_id.hdf5'):
 
 def prepare_data_file(filename = '../data/model_data.csv', nd = 3, dd = 3):
     data_array = np.genfromtxt(filename[0], delimiter=',')
-    data_array = np.concatenate((data_array, np.genfromtxt(filename[1], delimiter = ',')), axis = 0)
+    #data_array = np.concatenate((data_array, np.genfromtxt(filename[1], delimiter = ',')), axis = 0)
+    print(data_array.shape)
     signals = data_array[:, :11]
     max_signals = np.max(signals, axis = 0)
     for i in range(len(max_signals)):
@@ -148,7 +148,7 @@ def prepare_data_file(filename = '../data/model_data.csv', nd = 3, dd = 3):
 if __name__ == "__main__":
     # dd is dd+2
     # nd is nd
-    X, y = prepare_data_file([filename, filename1], nd=2, dd=2+2)
+    X, y = prepare_data_file([filename, filename1], nd=3, dd=3+2)
     if TRAIN:
         modelfile = neural_network_training(X, y)
     plot_sys_id(X, y)

test/tuning_gpc_controller.py
@@ -13,28 +13,27 @@ from target.target import Circle, Pringle,Pringle2, SingleAxisSineWave, SingleAx
 model_filename = str(os.environ['HOME']) + '/gpc_controller/test/sys_id.hdf5'
 
 NUM_EXPERIMENTS = 1
-NUM_TIMESTEPS = 500
+NUM_TIMESTEPS = 1000
 
-SCALE0 = 1.
-SCALE1 = 1.
+SCALE0 = 100.
+SCALE1 = 100.
 
 verbose = 1
 
 NNP = NeuralNetworkPredictor(model_file = model_filename,
-                    N1 = 0, N2 = 2, Nu = 2, nd = 2, dd = 2, K = 5,
-                    Q =     np.array([[50., 0.],
-                                     [0., 70.]]),
-                    Lambda = np.array([[1e-4, -5e-8],
-                                       [-5e-8, 0.07]]),
+                    N1 = 0, N2 = 2, Nu = 1, nd = 3, dd = 3, K = 5,
+                    Q =  np.array([[5., 0.],
+                                  [0., 1.]]),
+                    Lambda = np.array([[5e-7]]),
                         y0 = [0.0, 0.0, 0.0],
-                        u0 = [0.0, 0.0], s = 1e-20, b = 1e-3, r = 4e5)
+                        u0 = [0.0, 0.0], s = 1e-20, b = 1., r = 4.)
 
 NR_opt, Block = SolowayNR(d_model = NNP), \
                         BlockGym(vrpn_ip = "192.168.50.24:3883")
 log = Logger()
 Block.reset()
 
-#Block.step([0.0, 0.])
+Block.step([0., 0.])
 
 neutral_point = Block.get_state()
 NNP.y0  = neutral_point
@@ -43,9 +42,10 @@ print "neutral_point: ", neutral_point
 
 target = Pringle2(wavelength = 100, amplitude = 20./1000., center = neutral_point)
 
-Block.calibration_max = np.array([ 1, 288, 110,   1,   1,   1, 104,   1,   1,  38,   1 ])
+Block.calibration_max = np.array([ 80, 322, 109,   1,   1,   1, 102,   1,   1,  33,   1 ])
+#Block.get_signal_calibration()
 
-u_optimal_old = np.reshape([NNP.u0]*NNP.Nu, (-1, 2))
+u_optimal_old = np.reshape(NNP.u0*NNP.Nu, (-1, 2))
 new_state_new = copy.copy(neutral_point)
 del_u = np.zeros(u_optimal_old.shape)
 
@@ -76,24 +76,28 @@ try:
             NNP.yn = []
             ydeq = copy.copy(y_deque)
             for k in range(NNP.K):
-                neural_network_input = np.array((np.array(list(u_deque))).flatten().tolist() + \
+                neural_network_input = np.array((np.array(list(u_deque))/100.).flatten().tolist() + \
                                                     np.array(list(ydeq)
                                                         ).flatten().tolist() + \
                                                         signal).reshape(1, -1)
                 predicted_states = NNP.predict(neural_network_input).flatten()
-
                 NNP.yn += [predicted_states]
                 ydeq = roll_deque(ydeq, predicted_states.tolist())
 
+            y_deque = roll_deque(y_deque, predicted_states.tolist())
+
             NNP.last_model_input = neural_network_input
-            NNP.ym = target.spin(n, 0, NNP.K, 3, predicted_states.tolist())
+            NNP.ym = target.spin(n, 0, NNP.K, 3, neutral_point)
             new_state_old = new_state_new
 
-            u_optimal, del_u,  _ = NR_opt.optimize(u = np.array(u_optimal_old), delu = del_u, \
-                                        maxit = 2, rtol = 1e-4, verbose = True)
+            u_optimal, del_u,  _ = NR_opt.optimize(u = u_optimal_old, delu = del_u, \
+                                        maxit = 1, rtol = 1e-4, verbose = True)
 
             u_action = u_optimal[0, :].tolist()
             del_u_action = del_u[0, :].tolist()
+
+            #u_action[0] = np.cos(2.*np.pi*n/100.)
+            #u_action[1] = -0.5 #np.sin(2.*np.pi*n/100.)
 
             u_action[0] = np.clip(SCALE0*u_action[0],-100, 80)
             u_action[1] = np.clip(SCALE1*u_action[1],-100, 60)
@@ -111,7 +115,7 @@ try:
                         yn = predicted_states, ym = NNP.ym[0, :], \
                             elapsed = time.time()-seconds, u = u_action)
             if verbose == 1:
-                log.verbose(u_action = u_action)
+                log.verbose(u_action = u_action, elapsed = time.time() - seconds)
 
             log.log({str(e) : {'actual' : np.array(Block.get_state()).tolist(), \
                             'yn' : predicted_states.tolist(), \
@@ -120,8 +124,7 @@ try:
                             'u' : [u_action], \
                             'signal' : signal}})
 
-            u_optimal_old = u_optimal
-
+        u_optimal_old = np.reshape(NNP.u0*NNP.Nu, (-1, 2))
         Block.reset()
     log.plot_log()
     log.save_log()