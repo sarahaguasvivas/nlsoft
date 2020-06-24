b6cbbc74d361d873649861d982a04b81d55f705c
sarahaguasvivas <saag5228@colorado.edu>
2020-06-24 14:50:27 -0600

    improved plotting

--- test/training_sys_int.py
+++ test/training_sys_int.py
@@ -1,64 +1,72 @@
 #!/usr/bin/env python2.7
-
-#from gym.block_gym import * # vrpn not installed in mac
-#from controller.dynamic_model import *
-#from controller.newton_raphson import *
 import numpy as np
 import os
-from tensorflow.keras.constraints import max_norm
-
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 from tensorflow.keras import regularizers
 import keras
 from keras.models import Sequential, load_model
 from keras.layers import Dense, GaussianNoise, LSTM, BatchNormalization
+from keras.losses import Huber
 from sklearn.model_selection import train_test_split
 import keras.backend as K
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 import random
-NUM_DATA_RUNS = 100
+NUM_DATA_RUNS = 200
 TRAIN = False
 
-#plt.style.use('dark_background')
 plt.style.use('seaborn')
-filename = str(os.environ["HOME"]) + "/gpc_controller/data/model_data17.csv"
-filename1 = str(os.environ["HOME"]) + "/gpc_controller/data/model_data18.csv"
+filename = str(os.environ["HOME"]) + "/gpc_controller/data/model_data19.csv"
+#filename1 = str(os.environ["HOME"]) + "/gpc_controller/data/model_data18.csv"
 #filename2 = str(os.environ["HOME"]) + "/gpc_controller/data/model_data15.csv"
 #filename3 = str(os.environ["HOME"]) + "/gpc_controller/data/model_data16.csv"
 
+font = FontProperties()
+font.set_family('serif')
+font.set_name('Times New Roman')
+font.set_size(14)
+font = {'family' : 'serif',
+        'size': 14}
+
+plt.rcParams["font.family"] = "Times New Roman"
+
 def custom_loss(y_true, y_pred):
-    loss = 1000.*K.sqrt(K.sum(K.square(y_pred- y_true), axis = -1))
+    loss = K.sqrt(K.sum(K.square(y_pred- y_true), axis = -1))
     return loss
+
+h = Huber()
+def huber_loss(y_true, y_pred):
+    return 1000.*h(y_true, y_pred)
 
 keras.losses.custom_loss = custom_loss
 def neural_network_training(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4)
 
     model = Sequential()
-    model.add(Dense(10, activation='relu', kernel_initializer='random_normal',
+    model.add(Dense(20, activation='relu', kernel_initializer='random_normal',
                         kernel_regularizer = regularizers.l1(0.01),
                         activity_regularizer= regularizers.l2(0.02)))
     model.add(Dense(3,  activation = 'tanh', kernel_initializer='random_normal'))
 
-    #optimizer = keras.optimizers.Adam(lr = 0.01)
-    model.compile(optimizer= "adam", loss = custom_loss, metrics=['mse'])
+    model.compile(optimizer= "adam", loss = huber_loss, metrics=['mse'])
 
-    model.fit(X_train, y_train, epochs = 1000, batch_size = 1000, validation_split = 0.3)
+    model.fit(X_train, y_train, epochs = 200, batch_size = 64, validation_split = 0.2)
     print model.predict(X_test)
     model.save('sys_id.hdf5')
     return 'sys_id.hdf5'
 
 def plot_sys_id(X, y, modelfile= 'sys_id.hdf5'):
-
+    color_palette1 = ['#272838', '#F3DE8A', '#F3DE8A', '#F3DE8A']
+    color_palette = ['#1446A0', '#DB3069', '#F5D547', '#F5D547', '#3C3C3B']
     model = load_model(modelfile)
     y *= 1000 # convert meters to mm
     yn = 1000.*model.predict(X) #*1000
     plt.figure()
 
     lab = ['x', 'y', 'z']
-    L = 5000
+    L = yn.shape[0] // NUM_DATA_RUNS
 
     max_target = np.max(y, axis=0)
     min_target = np.min(y, axis=0)
@@ -66,26 +74,27 @@ def plot_sys_id(X, y, modelfile= 'sys_id.hdf5'):
 
     for i in range(3):
         plt.subplot(3, 2, 2*i+1)
-        plt.plot(yn[2:L, i] - shift[i], color = '#E74C3C',   label = r"$" + str(lab[i]) + "_{est}$")
-        plt.plot(y[2:L, i] - shift[i], color = '#5D6D7E',linestyle ='dashed', label = r"$" + str(lab[i]) + "_{true}$", alpha = 0.7)
-        plt.ylabel(str(lab[i]) + " [mm]")
+        plt.plot(yn[2:L, i] - shift[i], color = color_palette[0],   label = r"$" + str(lab[i]) + "_{est}$")
+        plt.plot(y[2:L, i] - shift[i], color = color_palette[1], label = r"$" + str(lab[i]) + "_{true}$", alpha = 0.7)
+        plt.ylabel(str(lab[i]) + " [mm]", **font)
 #        plt.title("Estimation vs. Truth for " + str(lab[i]) + " [mm]")
 
-        plt.legend()
+        plt.legend(prop={"family":"Times New Roman", "size": 14})
         if 2*i+1 == 1:
-            plt.title("Estimated vs. Ground Truth States")
+            plt.title("Estimated vs. Ground Truth States", **font)
         if 2*i+1 == 5:
-            plt.xlabel('timesteps')
-
+            plt.xlabel('timesteps', **font)
         plt.subplot(3, 2, 2*i+2)
-        plt.plot(y[1:L, i] - yn[1:L, i], color = '#34495E', linewidth = 0.5, label = r"$\Delta " + str(lab[i]) + " [mm]$")
+        plt.plot(y[2:L, i] - yn[2:L, i], color = '#34495E', linewidth = 0.5, label = r"$\Delta " + str(lab[i]) + " [mm]$")
 #        plt.title("Error in estimation for " + str(lab[i]) + " [mm]")
-        plt.ylabel(r"$\varepsilon_{" + str(lab[i]) + "}$ [mm]")
-        plt.ylim([-1., 1.])
-        plt.legend()
+        plt.ylabel(r"$\varepsilon_{" + str(lab[i]) + "}$ [mm]", **font)
+        plt.ylim([-10, 10.])
+        plt.legend(prop={"family": "Times New Roman", "size": 14})
         if 2*i+2 == 2:
-            plt.title("Errors in Testing Set Predictions")
-        plt.xlabel('timesteps')
+            plt.title("Errors in Testing Set Predictions", **font)
+        plt.xlabel('timesteps', **font)
+    plt.xticks(fontsize = 14)
+    plt.yticks(fontsize = 14)
     plt.show()
     plt.savefig("sysint", format ='svg', dpi = 1000)
 
@@ -93,15 +102,15 @@ def plot_sys_id(X, y, modelfile= 'sys_id.hdf5'):
     START = random.randint(0, yn.shape[0] - L)
     fig = plt.figure()
     ax = Axes3D(fig)
-    ax.plot3D(yn[START:START+L, 0], yn[START:START+L, 1], yn[START:START+L, 2],  \
+    ax.plot3D(yn[START:START+L, 0], yn[START:START+L, 1], yn[START:START+L, 2],
                             linewidth = 3, alpha = 0.9, label = "estimated position")
-    ax.plot3D(y[START:START + L, 0], y[START:START+L, 1], y[START:START+L, 2], \
+    ax.plot3D(y[START:START + L, 0], y[START:START+L, 1], y[START:START+L, 2],
                             alpha = 1, linewidth = 3, label = "ground truth")
     ax.set_aspect('equal')
     #ax.set_xlim(-.04, .05)
     #ax.set_ylim(-.1, .05)
     #ax.set_zlim(-.05, .05)
-    plt.legend()
+    plt.legend(prop={"family": "Times New Roman", "size": 14})
     plt.xlabel('x [mm]')
     plt.ylabel('y [mm]')
     plt.title('Estimated position vs. Ground Truth')
@@ -113,9 +122,8 @@ def plot_sys_id(X, y, modelfile= 'sys_id.hdf5'):
     y_pred= 1000.*model.predict(X_test)
 
     print y_pred, y_test, y_pred.shape
-    diff = np.linalg.norm(y_test-y_pred)
-
-    print "Testing set avg l2 norm:", diff
+    diff = np.linalg.norm(y_test-y_pred, axis = 1)
+    print "Testing set avg l2 norm:", np.mean(diff)
 
 
 def prepare_data_file(filename = '../data/model_data.csv', nd = 5, dd = 5):
@@ -149,16 +157,13 @@ def prepare_data_file(filename = '../data/model_data.csv', nd = 5, dd = 5):
     U = np.deg2rad(U[:, 2:])
     S = signals[N - 1:, :]
 
+    print "Y", Y
     Y = Y[:, 3:-3] # Y
-    min_y = np.min(Y, axis =0)
-    max_y = np.max(Y, axis =0)
-    shift = (min_y + max_y)/2.
-    Y -= shift
-    print shift
+
     X = np.concatenate((U, Y[:, 3:]), axis = 1)
     X = np.concatenate((X, S), axis = 1)
     y = Y[:, :3] # we are using all of this to estimate current
-    print y.shape
+
     print np.cov(y.T, bias = True)
     return X, y
 
@@ -166,7 +171,7 @@ def prepare_data_file(filename = '../data/model_data.csv', nd = 5, dd = 5):
 if __name__ == "__main__":
     # dd is dd+2
     # nd is nd
-    X, y = prepare_data_file([filename, filename1], nd=3, dd=3+2)
+    X, y = prepare_data_file([filename], nd=3, dd=3+2)
     if TRAIN:
         modelfile = neural_network_training(X, y)
     plot_sys_id(X, y)
