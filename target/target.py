95efc54b81e1d9e75709c827d81b70a051952295
sarahaguasvivas <saag5228@colorado.edu>
2020-06-25 21:25:30 -0600

    new model

--- target/target.py
+++ target/target.py
@@ -35,7 +35,7 @@ class Circle(Target):
 
         for _ in range(n1, n2):
             y = self.center[1] + self.amplitude * np.sin(2.*np.pi*(timestep + i) \
-                                                        / self.wavelength + phase) #- 10./1000.
+                                                        / self.wavelength + phase) - 10./1000.
             z = self.center[2] + self.amplitude*np.cos(2*np.pi*(timestep + i)/ \
                                                         self.wavelength + phase) + 0./1000.
             x = self.center[0]
@@ -66,7 +66,7 @@ class Pringle2:
                                                         / (self.wavelength) + phase + 0.00001*(timestep + i)**2) \
                                                                     - 10./1000.
             z = self.amplitude * np.cos(2*np.pi*(timestep + i ) / \
-                                            (self.wavelength) + phase + 0.00001*(timestep + i)**2) + 0./1000.
+                                            (self.wavelength) + phase + 0.00001*(timestep + i)**2) - 0./1000.
             x = self.amplitude * np.sin(100*z*y) - 0./1000.
 
             target[i, :] = [self.center[0] + x, self.center[1] + y, self.center[2] + z]
