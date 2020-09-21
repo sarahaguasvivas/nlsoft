6d9c4b1b989e98528420497439a53afe6fc8588d
sarahaguasvivas <saag5228@colorado.edu>
2020-09-21 14:51:52 -0600

    tuned new controller

--- python/test/controller_recursive.py
+++ python/test/controller_recursive.py
@@ -18,11 +18,11 @@ verbose = 1
 NNP = RecursiveNeuralNetworkPredictor(model_file = model_filename,
                                       N1 = 0, N2 = 2, Nu = 1,
                                       nd = 5, dd = 5, K = 5,
-                                      Q = np.array([[100., 0., 0],
+                                      Q = np.array([[1., 0., 0],
                                                     [0., 1000., 0],
                                                     [0., 0., 100.]]),
                                       Lambda = np.array([[1., 0.],
-                                                         [0., 6e-1]]),
+                                                         [0., 5e-1]]),
                                       s = 1e-20, b = 1e-3, r = 4.,
                                       states_to_control = [1, 1, 1],
                                       x0 = [0.0, 0.0, 0.0],
@@ -43,7 +43,7 @@ target = FigureEight(a = 20. / 1000., b = 5./1000., wavelength= 100.,
                      center = neutral_point)
 
 #Block.get_signal_calibration()
-Block.calibration_max = np.array([ 78., 1, 14.,   1,   1,   120., 163.,   1,   1,  1,  17.])
+Block.calibration_max = np.array([ 40., 1, 12.,   1,   1,   128., 170.,   1,   1,  1,  16.])
 
 u_optimal_old = np.reshape(NNP.u0 * NNP.nu, (-1, 2))
 del_u = np.zeros(u_optimal_old.shape)
