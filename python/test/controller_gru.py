1927ccfbdb1818f96b272136cc7b9ca5efac1d66
sarahaguasvivas <saag5228@colorado.edu>
2020-09-29 12:58:05 -0600

    working GRU controller

--- python/test/controller_gru.py
+++ python/test/controller_gru.py
@@ -56,85 +56,85 @@ log.log({'metadata' : {'neutral_point' : neutral_point,
 u_deque = deque()
 y_deque = deque()
 
-#try:
-for e in range(NUM_EXPERIMENTS):
-    log.log({str(e) : {'predicted' : [], 'actual' : [], 'yn' : [],
-            'ym' : [], 'elapsed' : [], 'u' : []}})
+try:
+    for e in range(NUM_EXPERIMENTS):
+        log.log({str(e) : {'predicted' : [], 'actual' : [], 'yn' : [],
+                'ym' : [], 'elapsed' : [], 'u' : []}})
 
+        Block.reset()
+        time.sleep(1)
+        NNP.x0 = Block.get_state()
+        u_deque.clear()
+        y_deque.clear()
+
+        u_deque, y_deque = first_load_deques(NNP.x0, NNP.u0, NNP.nd, NNP.dd)
+        u_action, predicted_states = np.array(NNP.u0), np.array(NNP.x0)
+
+        target.center = NNP.x0
+        for n in range(NUM_TIMESTEPS):
+            seconds = time.time()
+            signal = np.divide(Block.get_observation(), Block.calibration_max,
+                                dtype = np.float64).tolist()
+
+            NNP.yn = []
+            ydeq = y_deque.copy()
+            for k in range(NNP.K):
+                neural_network_input = np.array((np.array(list(u_deque))).flatten().tolist() + \
+                                np.array(list(ydeq)).flatten().tolist() + signal).reshape(1, -1, 1)
+
+                predicted_states = NNP.predict(neural_network_input).flatten()
+
+                NNP.yn += [(NNP.C @ predicted_states).tolist()]
+                ydeq = roll_deque(ydeq, predicted_states.tolist())
+
+            y_deque = roll_deque(y_deque, predicted_states.tolist())
+
+            NNP.last_model_input = neural_network_input
+
+            target_path = target.spin(n, 0, NNP.K, 3)
+
+            NNP.ym = (NNP.C @ target_path.T).reshape(NNP.ny, -1).T.tolist()
+
+            u_optimal, del_u,  _ = NR_opt.optimize(u = u_optimal_old, delu = del_u,
+                                        maxit = 1, rtol = 1e-4, verbose = True)
+
+            u_action = u_optimal[0, :].tolist()
+            del_u_action = del_u[0, :].tolist()
+
+            u_action[0] = np.clip(np.rad2deg(u_action[0]), -100., 50.)
+            u_action[1] = np.clip(np.rad2deg(u_action[1]), -100., 50.)
+
+            Block.step(action = u_action)
+
+            NNP.update_dynamics(u_optimal[0, :].tolist(), del_u_action,
+                        predicted_states.tolist(), target_path[0, :].tolist())
+
+            u_optimal_old = u_optimal
+            u_deque = roll_deque(u_deque, u_optimal[0, :].tolist())
+            elapsed = time.time()-seconds
+            actual_ = np.array(Block.get_state()).tolist()
+            if verbose == 0:
+                log.verbose( actual = actual_,
+                            yn = predicted_states, ym =target_path[0, :],
+                            elapsed = elapsed, u = u_action)
+            if verbose == 1:
+                log.verbose(u_action = u_action, elapsed = time.time() - seconds)
+
+            log.log({str(e) : {'actual' : actual_,
+                            'yn' : predicted_states.tolist(),
+                            'ym' : target_path[0, :].tolist(),
+                            'elapsed' : elapsed,
+                            'u' : [u_action],
+                            'signal' : signal}})
+
+        u_optimal_old = np.reshape(NNP.u0 * NNP.nu, (-1, 2))
+        Block.reset()
+    log.plot_log()
+    log.save_log(filename = logfile_name)
+
+except Exception as e:
+    print(str(e))
+    print("Closing all connections!")
     Block.reset()
-    time.sleep(1)
-    NNP.x0 = Block.get_state()
-    u_deque.clear()
-    y_deque.clear()
-
-    u_deque, y_deque = first_load_deques(NNP.x0, NNP.u0, NNP.nd, NNP.dd)
-    u_action, predicted_states = np.array(NNP.u0), np.array(NNP.x0)
-
-    target.center = NNP.x0
-    for n in range(NUM_TIMESTEPS):
-        seconds = time.time()
-        signal = np.divide(Block.get_observation(), Block.calibration_max,
-                            dtype = np.float64).tolist()
-
-        NNP.yn = []
-        ydeq = y_deque.copy()
-        for k in range(NNP.K):
-            neural_network_input = np.array((np.array(list(u_deque))).flatten().tolist() + \
-                            np.array(list(ydeq)).flatten().tolist() + signal).reshape(1, -1, 1)
-
-            predicted_states = NNP.predict(neural_network_input).flatten()
-
-            NNP.yn += [(NNP.C @ predicted_states).tolist()]
-            ydeq = roll_deque(ydeq, predicted_states.tolist())
-
-        y_deque = roll_deque(y_deque, predicted_states.tolist())
-
-        NNP.last_model_input = neural_network_input
-
-        target_path = target.spin(n, 0, NNP.K, 3)
-
-        NNP.ym = (NNP.C @ target_path.T).reshape(NNP.ny, -1).T.tolist()
-
-        u_optimal, del_u,  _ = NR_opt.optimize(u = u_optimal_old, delu = del_u,
-                                    maxit = 1, rtol = 1e-4, verbose = True)
-
-        u_action = u_optimal[0, :].tolist()
-        del_u_action = del_u[0, :].tolist()
-
-        u_action[0] = np.clip(np.rad2deg(u_action[0]), -100., 50.)
-        u_action[1] = np.clip(np.rad2deg(u_action[1]), -100., 50.)
-
-        #Block.step(action = u_action)
-
-        NNP.update_dynamics(u_optimal[0, :].tolist(), del_u_action,
-                    predicted_states.tolist(), target_path[0, :].tolist())
-
-        u_optimal_old = u_optimal
-        u_deque = roll_deque(u_deque, u_optimal[0, :].tolist())
-        elapsed = time.time()-seconds
-        actual_ = np.array(Block.get_state()).tolist()
-        if verbose == 0:
-            log.verbose( actual = actual_,
-                        yn = predicted_states, ym =target_path[0, :],
-                        elapsed = elapsed, u = u_action)
-        if verbose == 1:
-            log.verbose(u_action = u_action, elapsed = time.time() - seconds)
-
-        log.log({str(e) : {'actual' : actual_,
-                        'yn' : predicted_states.tolist(),
-                        'ym' : target_path[0, :].tolist(),
-                        'elapsed' : elapsed,
-                        'u' : [u_action],
-                        'signal' : signal}})
-
-    u_optimal_old = np.reshape(NNP.u0 * NNP.nu, (-1, 2))
-    Block.reset()
-log.plot_log()
-log.save_log(filename = logfile_name)
-
-#except Exception as e:
-#    print(str(e))
-#    print("Closing all connections!")
-#    Block.reset()
-#    Block.motors.close_connection()
+    Block.motors.close_connection()
 
