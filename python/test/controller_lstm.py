1d8dc88bfc062ef5f25c7354252e0f269e614e15
sarahaguasvivas <saag5228@colorado.edu>
2020-12-10 13:45:08 -0700

    tuned lstm

--- python/test/controller_lstm.py
+++ python/test/controller_lstm.py
@@ -18,15 +18,15 @@ savelog = True
 NNP = RecursiveNeuralNetworkPredictor(model_file = model_filename,
                                       N1 = 0, N2 = 1, Nu = 1,
                                       nd = 2, dd = 2, K = 2,
-                                      Q = np.array([[1., 0., 0],
-                                                    [0., 1e4, 0.],
-                                                    [0., 0., 6e3]]),
-                                      Lambda = np.array([[1., 0.],
+                                      Q = np.array([[1e1, 0., 0],
+                                                    [0., 2e4, 0.],
+                                                    [0., 0., 5e3]]),
+                                      Lambda = np.array([[5., 0.],
                                                          [0., 1.]]),
                                       s = 1e-20, b = 1e-5, r = 4e3,
                                       states_to_control = [1, 1, 1],
                                       y0= [0.0, 0.0, 0.0],
-                                      u0 = [np.deg2rad(-50.), np.deg2rad(-50.)],
+                                      u0 = [np.deg2rad(-70.), np.deg2rad(-50.)],
                                       step_size = 8e-2)
 
 NR_opt, Block = SolowayNR(d_model = NNP), BlockGym(vrpn_ip = "192.168.50.24:3883")
@@ -45,7 +45,7 @@ NNP.y0 = neutral_point
 
 target = FigureEight(a = 8./1000., b = 15./1000., wavelength = 400., center = neutral_point)
 
-Block.calibration_max = np.array([613., 134., 104., 200., 128., 146., 183., 1., 1., 7., 60])
+Block.calibration_max = np.array([613., 134., 150., 200., 128., 146., 183., 1., 2., 50., 100])
 
 u_optimal_old = np.reshape(NNP.u0 * NNP.nu, (-1, 2))
 del_u = np.zeros(u_optimal_old.shape)
