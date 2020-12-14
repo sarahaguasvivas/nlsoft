24aac4c8af45df0c04818fa017aa64ec8f228512
sarahaguasvivas <saag5228@colorado.edu>
2020-12-13 18:12:00 -0700

    gru ready for figure8

--- python/target/target.py
+++ python/target/target.py
@@ -220,7 +220,7 @@ class FigureEight:
         target = np.empty([n2-n1, dims])
         i = 0
         for _ in range(n1, n2):
-            y = self.a * np.sin((timestep + i) / self.wavelength)  - 5./1000.
+            y = self.a * np.sin((timestep + i) / self.wavelength)  - 0./1000.
             z = self.b * np.sin((timestep + i) / self.wavelength) * \
                np.cos((timestep + i)/self.wavelength) + 0./1000.
             x = 0.004*np.sin((timestep+i) / (self.wavelength))**2 - 0./1000.
@@ -228,18 +228,18 @@ class FigureEight:
             del_X = [x, y, z]
 
             #rotation = np.array([-np.pi/3., -np.pi/4., -np.pi/6.])
-            rotation = np.array([0., 0.15, np.pi/4.])
+            rotation = np.array([0., 0., .5])
             rot = R.from_rotvec(rotation)
             del_X = rot.apply(del_X)
 
             target[i, :] = [del_X[0] + self.center[0] + 0./1000.,
-                            del_X[1] + self.center[1],
+                            del_X[1] + self.center[1] + 0./1000.,
                             del_X[2] + self.center[2] + 0./1000.]
             #target[i, :] = rot.apply(target[i, :])
 
             target[i, :] = [target[i, 0]  + 0./1000.,
                             target[i, 1]  + 0./1000.,
-                            target[i, 2]  + 10./1000.]
+                            target[i, 2]  + 8./1000.]
             i+=1
         return target
 
--- python/test/controller_gru.py
+++ python/test/controller_gru.py
@@ -19,13 +19,13 @@ savelog = False
 
 NNP = RecursiveNeuralNetworkPredictor(model_file = model_filename,
                                       N1 = 0, N2 = 1, Nu = 1,
-                                      nd = 2, dd = 2, K = 3,
-                                      Q = np.array([[1e4, 0., 0],
+                                      nd = 2, dd = 2, K = 2,
+                                      Q = np.array([[6e4, 0., 0],
                                                     [0., 1e4, 0.],
-                                                    [0., 0., 1e4]]),
+                                                    [0., 0., 5e4]]),
                                       Lambda = np.array([[1., 0.],
-                                                         [0., 1.]]),
-                                      s = 1e-20, b = 1e-5, r = 4e3,
+                                                         [0., 5.]]),
+                                      s = 1e-20, b = 1e-8, r = 4e5,
                                       states_to_control = [1, 1, 1],
                                       y0= [0.0, 0.0, 0.0],
                                       u0 = [np.deg2rad(-60.), np.deg2rad(-50.)],
@@ -59,12 +59,12 @@ NNP.y0 = neutral_point
 
 #target = Diagonal(wavelength = 15000, amplitude= 10./1000., center = neutral_point)
 
-target = FigureEight(a = 12./1000., b = 20./1000., wavelength = 400., center = neutral_point)
+target = FigureEight(a = 10./1000., b = 20./1000., wavelength = 400., center = neutral_point)
 #target = Pringle(wavelength = 1000, amplitude = 5./1000.,
 #                                center = neutral_point)
 
 #Block.calibration_max = np.array([613., 134., 104., 200., 128., 146., 183., 1., 1., 7., 60])
-Block.calibration_max = np.array([613., 134., 104., 200., 128., 146., 183., 1., 2., 7., 100])
+Block.calibration_max = np.array([613., 134., 104., 200., 128., 146., 183., 1., 2., 7., 100.])
 
 u_optimal_old = np.reshape(NNP.u0 * NNP.nu, (-1, 2))
 del_u = np.zeros(u_optimal_old.shape)
@@ -120,8 +120,8 @@ try:
             u_action = u_optimal[0, :].tolist()
             del_u_action = del_u[0, :].tolist()
 
-            u_action[0] = np.clip(1.*(np.rad2deg(u_action[0]) + 50.) - 50. - 10., -100., 50.)
-            u_action[1] = np.clip(1.*(np.rad2deg(u_action[1]) + 50.) - 50. - 0., -100., 50.)
+            u_action[0] = np.clip(1.*(np.rad2deg(u_action[0]) + 50.) - 50. - 15., -100., 50.)
+            u_action[1] = np.clip(1.*(np.rad2deg(u_action[1]) + 50.) - 50. - 12., -100., 50.)
 
             #u_action[0] = ((1.+np.cos(2.* np.pi / 1000. * n))/2. * 150. - 100.)
             #u_action[1] = ((1.+np.sin(2.* np.pi / 1000. * n))/2. * 150. - 100.)
