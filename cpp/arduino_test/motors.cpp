c64ffdd67e09ffe34611f5f617d0e67e3c1b1bbc
sarahaguasvivas <saag5228@colorado.edu>
2021-02-26 11:27:32 -0700

    motors.cpp is an SPI master now

--- cpp/arduino_test/motors.cpp
+++ cpp/arduino_test/motors.cpp
@@ -1,5 +1,20 @@
 #include "motors.hpp"
+#define OLDL_MIN -100
+#define OLDL_MAX 50
+#define NEW_MIN 0
+#define NEW_MAX 600
 
+/* 
+ *  This module is responsible of sending the motor commands through SPI to the OpenCM board
+ */
+int convert_mapped_values(float angle)
+{
+    int new_angle;
+
+    new_angle = (int)((angle - OLD_MIN)/(OLD_MAX - OLD_MIN)*(NEW_MAX - NEW_MIN) + NEW_MIN);
+    
+    return new_angle;
+}
 
 void step_motor(float * u, int m)
 {
@@ -8,4 +23,20 @@ void step_motor(float * u, int m)
    *  u is the optimal control input matrix
    *  m is the number of motors
    */
+   
+   char buf;
+   
+   digitalWrite(SS, LOW);
+   SPI.transfer(1);
+
+   for (int pos=0; pos < sizeof(buf) - 1; pos++)
+   {
+      buf[pos] = SPI.transfer(0);
+      if (buf[pos] == 0){
+        break;
+      }
+   }
+   buf[sizeof (buf) - 1] = 0;
+   digitalWrite(SS, HIGH);
+   
 }
--- cpp/arduino_test/motors.hpp
+++ cpp/arduino_test/motors.hpp
@@ -1,9 +1,20 @@
+/* 
+ *  This module is responsible of sending the motor commands through SPI to the OpenCM board
+ */
+
 #ifndef __MOTORS_H__
 #define __MOTORS_H__
 
 #include <stdlib.h>
+#include <SPI.h>
+
+digitalWrite(SS, HIGH);
+SPI.begin();
+SPI.setClockDivider(SPI_CLOCK_DIV8);
 
 void step_motor(float *, int);
+
+int convert_mapped_values(float);
 
 
 #endif
