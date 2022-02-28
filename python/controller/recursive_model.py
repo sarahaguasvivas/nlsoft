fixed Hessian

python/controller/recursive_model.py
@@ -399,7 +399,7 @@ class RecursiveNeuralNetworkPredictor():
                     #                         self.b - u[j, i], 2.0))
                 jacobian[j, :] += sub_sum
         jacobian += np.array(sum_output).reshape(self.nu, -1)
-        print(jacobian)
+        print("jacobian", jacobian)
         return jacobian
 
     def compute_cost(self):

python/test/controller_gru_vani.py
@@ -19,7 +19,7 @@ verbose = 1
 savelog = True
 
 NNP = RecursiveNeuralNetworkPredictor(model_file = model_filename,
-                                      N1 = 0, N2 = 1, Nu= 1,
+                                      N1 = 0, N2 = 3, Nu= 1,
                                       nd = 2, dd = 2, K = 3,
                                       Q = np.array([[1e3, 0., 0.],
                                                     [0., 1e3, 0.],