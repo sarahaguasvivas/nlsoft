fixed hessian

python/controller/recursive_model.py
@@ -328,9 +328,9 @@ class RecursiveNeuralNetworkPredictor():
 
         shape[0] = len(x)
         shape = tuple(shape)
-        hessian = (self.model.predict(x_p.reshape(shape), batch_size=len(x)).flatten() - \
-                   2. * self.model.predict(xx.reshape(shape), batch_size=len(x)).flatten() +
-                   self.model.predict(x_m.reshape(shape), batch_size=len(x)).flatten()) / \
+        hessian = (self.model.predict(x_p.reshape(shape), batch_size=len(x)) - \
+                   2. * self.model.predict(xx.reshape(shape), batch_size=len(x)) +
+                   self.model.predict(x_m.reshape(shape), batch_size=len(x))) / \
                   (h * h)
         return np.array(hessian)
 
@@ -361,11 +361,14 @@ class RecursiveNeuralNetworkPredictor():
             TODO: Fixme
         """
         second_gradient = self.num_grad_grad(self.input_vector)
-        second_gradient = second_gradient.reshape(-1, self.input_vector.shape[1], self.nx)
-        second_gradient = second_gradient[:self.nd * self.m, :self.nd*self.m, :]
-        second_gradient = second_gradient.reshape(self.m, self.nx, self.nd)
-        second_gradient = second_gradient.sum(axis = -1).sum(axis = 0)
-        return second_gradient
+        #second_gradient = second_gradient.reshape(-1, self.input_vector.shape[1], self.nx)
+        #second_gradient = second_gradient[:self.nd * self.m, :self.nd*self.m, :]
+        #second_gradient = second_gradient.reshape(self.m, self.nx, self.nd)
+        #second_gradient = second_gradient.sum(axis = -1).sum(axis = 0)
+        second_gradient = second_gradient[:self.nd * self.m, :]
+        second_gradient = second_gradient.reshape(self.m, -1, self.nx)
+        second_gradient = np.sum(second_gradient, axis = 1)
+        return second_gradient.T
 
     def jacobian(self, u, del_u):
         y, ym = self.get_computation_vectors()